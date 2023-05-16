4495b48bfa4c66744f086aa5ca306524a4c41412
Leonardo Rothe Tagliafico <leo_rothe@msn.com>
2023-05-15 21:05:19 -0500

    Codificar caracteres en español

--- inline-css/index.php
+++ inline-css/index.php
@@ -4,17 +4,42 @@ use TijsVerkoyen\CssToInlineStyles\CssToInlineStyles;
 
 require_once __DIR__ . "/vendor/autoload.php";
 
+header("Content-Type: text/html; charset=utf-8");
+
+function encodeSpanishCharacters($htmlString) {
+	return str_replace(['&lt;', '&gt;'], ['<', '>'], htmlentities($htmlString, ENT_NOQUOTES, "UTF-8"));
+	$spanishCharacters = array(
+		'á' => '&aacute;',
+		'é' => '&eacute;',
+		'í' => '&iacute;',
+		'ó' => '&oacute;',
+		'ú' => '&uacute;',
+		'ñ' => '&ntilde;',
+		'Á' => '&Aacute;',
+		'É' => '&Eacute;',
+		'Í' => '&Iacute;',
+		'Ó' => '&Oacute;',
+		'Ú' => '&Uacute;',
+		'Ñ' => '&Ntilde;'
+	);
+	return preg_replace_callback('#([áéíóúñÁÉÍÓÚÑ])#', function ($m) use ($spanishCharacters) {
+		return $spanishCharacters[$m[0]] ?? "XXX";
+	}, $htmlString);
+}
+
 if (isset($_FILES["html"]) && isset($_FILES["css"])) {
 	$html = file_get_contents($_FILES["html"]["tmp_name"]);
 	$css = file_get_contents($_FILES["css"]["tmp_name"]);
 
 	$cssToInlineStyles = new CssToInlineStyles();
 	$result = $cssToInlineStyles->convert($html, $css);
+	$result = encodeSpanishCharacters($result);
 } else {
 	$result = false;
 }
 
-?><!doctype html>
+?>
+<!doctype html>
 <html>
 
 <head>
@@ -38,13 +63,13 @@ if (isset($_FILES["html"]) && isset($_FILES["css"])) {
 		</footer>
 	</form>
 
-	<?php if ($result): ?>
-	<section>
-		<h1>Resultado</h1>
-		<textarea rows="20">
-			<?php echo $result ?>
-		</textarea>
-	</section>
+	<?php if ($result) : ?>
+		<section>
+			<h1>Resultado</h1>
+			<p>
+				<a download href="data:text/html;base64,<?php echo base64_encode($result) ?>">Descargar</a>
+			</p>
+		</section>
 	<?php endif ?>
 </body>
 
